e5b15c028041899749a2c348421afca9a13c3a3e
edureka <support@edureka.co>
2023-12-12 03:30:29 +0000

    Third commit of Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
      public static void main(String[] args) {
      System.out.println("Welcome to Git's Online Training!");
+     System.out.println("Welcome to Edureka!");
      }
 }
