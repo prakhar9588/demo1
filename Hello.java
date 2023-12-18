1c87a2d432a57ba93deba09df5848331aabfc5ee
Prakhar <148931558+prakhar9588@users.noreply.github.com>
2023-12-18 19:09:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
      public static void main(String[] args) {
-     System.out.println("Welcome to Jenkins's Online Training!");
+     System.out.println("Welcome to Jenkins's offline Training!");
          }
 }
